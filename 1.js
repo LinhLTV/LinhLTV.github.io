0b451d715cc643c58bbce2115d51121f6210316e
LinhLTV <lthuc600@gmail.com>
2020-06-06 23:36:30 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	var nut1 = document.querySelector('.nut1');
+	var nut2 = document.querySelector('.nut2');
+	var menudown = document.querySelector('.menudown');
+	nut1.addEventListener('click',function(){
+		this.classList.add('hien');
+		nut2.classList.add('close');
+		menudown.classList.add('down');
+	},false);
+	nut2.addEventListener('click',function(){
+		nut1.classList.remove('hien');
+		nut2.classList.remove('close');
+		menudown.classList.remove('down');
+
+	},false);
+	
+
+},false);
